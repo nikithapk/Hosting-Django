a17e529498b0941327c70c9e5d4175d84f082fe5
nikithapk <nikithapoolanda@gmail.com>
2020-02-22 13:18:25 +0530

    "first commit"

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\NP068165\OneDrive - Cerner Corporation\Desktop\GemSites\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
